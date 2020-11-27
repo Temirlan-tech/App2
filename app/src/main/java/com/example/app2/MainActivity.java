886072ad7ccaf06a2132af70aad3deeff0177691
Temirlan <t.toktosunov@list.ru>
2020-11-27 20:16:18 +0600

    Добавил блок для портретного режима

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupUi();
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public void setupUi() {
